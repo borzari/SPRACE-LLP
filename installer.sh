035f0e9a5de6e1e4cbda877cc4538e82b2576f79
Andre Lessa <lessa.a.p@gmail.com>
2021-01-18 13:51:15 -0300

    Updated installer

--- installer.sh
+++ installer.sh
@@ -2,12 +2,24 @@
 
 homeDIR="$( pwd )"
 
-
+echo "[Checking system dependencies]"
+PKG_OK=$(dpkg-query -W -f='${Status}' autoconf 2>/dev/null | grep -c "ok installed")
+if test $PKG_OK = "0" ; then
+  echo "autoconf not found. Install it with sudo apt-get install autoconf."
+  exit
+fi
+PKG_OK=$(dpkg-query -W -f='${Status}' libtool 2>/dev/null | grep -c "ok installed")
+if test $PKG_OK = "0" ; then
+  echo "libtool not found. Install it with sudo apt-get install libtool."
+  exit
+fi
 PKG_OK=$(dpkg-query -W -f='${Status}' gzip 2>/dev/null | grep -c "ok installed")
 if test $PKG_OK = "0" ; then
   echo "gzip not found. Install it with sudo apt-get install gzip."
   exit
 fi
+
+
 
 
 madgraph="MG5_aMC_v2.8.2.tar.gz"
@@ -16,54 +28,15 @@ echo -n "Install MadGraph (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
 	mkdir MG5;
-	echo "[installer] fetching MadGraph5"; wget $URL 2>/dev/null || curl -O $URL; tar -zxf $madgraph -C MG5 --strip-components 1;
+	echo "[installer] getting MadGraph5"; wget $URL 2>/dev/null || curl -O $URL; tar -zxf $madgraph -C MG5 --strip-components 1;
 	cd $homeDIR
 	rm $madgraph;
-#	echo "[installer] replacing MadGraph files with fixes";
+	echo "[installer] replacing MadGraph files with fixes";
 #    cp ./madgraphfixes/mg5_configuration.txt MG5/input/;
 #    cp ./madgraphfixes/madgraph_interface.py MG5/madgraph/interface/;
+#    cp ./madgraphfixes/diagram_generation.py MG5/madgraph/core/;
+
 fi
-
-#Get FastJet tarball
-fastjet="fastjet-3.3.4"
-URL=http://fastjet.fr/repo/$fastjet.tar.gz
-echo -n "Install FastJet (y/n)? "
-read answer
-if echo "$answer" | grep -iq "^y" ;then
-	mkdir fastjet_tmp
-	echo "[installer] fetching Fastjet"; wget $URL 2>/dev/null || curl -O $URL; tar -zxf $fastjet.tar.gz -C fastjet_tmp;
-	mkdir $fastjet;
-	echo "Installing Fastjet in $fastjet";
-	cd fastjet_tmp/$fastjet;
-	./configure  --enable-allplugins --prefix=$homeDIR/$fastjet/;
-	make install;
-
-	#Clean up
-	cd $homeDIR
-	rm -rf fastjet_tmp; rm $fastjet.tar.gz;
-fi
-
-#Get HepMC tarball
-hepmc="hepmc2.06.11"
-echo -n "Install HepMC (y/n)? "
-read answer
-if echo "$answer" | grep -iq "^y" ;then
-	mkdir hepMC_tmp
-	URL=http://hepmc.web.cern.ch/hepmc/releases/$hepmc.tgz
-	echo "[installer] getting HepMC"; wget $URL 2>/dev/null || curl -O $URL; tar -zxf $hepmc.tgz -C hepMC_tmp --strip-components 1;
-	mkdir $hepmc; mkdir $hepmc/build; mkdir HepMC;
-	echo "Installing HepMC in ./HepMC";
-	cd $hepmc/build;
-	../../hepMC_tmp/configure --prefix=$homeDIR/HepMC --with-momentum=GEV --with-length=MM;
-	make;
-	make check;
-	make install;
-
-	#Clean up
-	cd $homeDIR;
-	rm -rf hepMC_tmp; rm $hepmc.tgz;
-fi
-
 
 #Get pythia tarball
 pythia="pythia8303.tgz"
@@ -73,10 +46,10 @@ read answer
 if echo "$answer" | grep -iq "^y" ;then
 	if hash gzip 2>/dev/null; then
 		mkdir pythia8;
-		echo "[installer] fetching Pythia"; wget $URL 2>/dev/null || curl -O $URL; tar -zxf $pythia -C pythia8 --strip-components 1;
+		echo "[installer] getting Pythia"; wget $URL 2>/dev/null || curl -O $URL; tar -zxf $pythia -C pythia8 --strip-components 1;
 		echo "Installing Pythia in pythia8";
 		cd pythia8;
-		./configure --with-root=$ROOTSYS --prefix=$homeDIR/pythia8 --with-gzip --with-fastjet3=../$fastjet --with-hepmc2=$homeDIR/HepMC
+		./configure --with-root=$ROOTSYS --prefix=$homeDIR/pythia8 --with-gzip
 		make -j4; make install;
 		cd $homeDIR
 		rm $pythia;
@@ -85,14 +58,38 @@ if echo "$answer" | grep -iq "^y" ;then
 	fi
 fi
 
+
 echo -n "Install Delphes (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
-	echo "[installer] fetching Delphes";
+	echo "[installer] getting Delphes";
   git clone https://github.com/delphes/delphes.git Delphes;
   cd Delphes;
+  export PYTHIA8=$homeDIR/pythia8;
   echo "[installer] installing Delphes";
-  make -j4;
+  make HAS_PYTHIA8=true;
 	cd $homeDIR;
 fi
 
+
+echo -n "Install CheckMATE (y/n)? "
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+  echo "[installer] getting CheckMATE";
+  git clone git@github.com:CheckMATE2/checkmate2.git CheckMATE2;
+  cd CheckMATE2;
+  rm -rf .git
+  autoreconf -i -f;
+  ./configure --with-rootsys=$ROOTSYS --with-delphes=$homeDIR/Delphes --with-pythia=$homeDIR/pythia8 --with-madgraph=$homeDIR/MG5
+  echo "[installer] installing CheckMATE";
+  make -j4
+       cd $homeDIR
+  echo "[installer] Adding new analyses to CheckMATE";
+#  cp -r myCheckMateFiles/tools/* CheckMATE2/tools/;
+#  cp -r myCheckMateFiles/data/* CheckMATE2/data/;
+  cd CheckMATE2;
+  echo "[installer] recompiling CheckMATE";
+  make;
+  cd $homeDIR
+
+fi
